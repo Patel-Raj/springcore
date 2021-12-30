e5989a0922d07bc083e3079eeb6453a3b169b24e
Raj Patel <patelraj22777@gmail.com>
2021-12-30 21:17:19 +0530

    Removed errors from App.java

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -7,7 +7,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
-    Logger
     public static void main(String args[]) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
         Student student = (Student) applicationContext.getBean("student1");
