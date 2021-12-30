851ae7dc91f35395a0123de37c4fca1e5b7343dd
Raj Patel <patelraj22777@gmail.com>
2021-12-30 19:15:35 +0530

    Created Srudent POJO

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private int sutdentId;
+    private String studentName;
+    private String studentAddress;
+
+    public Student(int sutdentId, String studentName, String studentAddress) {
+        this.sutdentId = sutdentId;
+        this.studentName = studentName;
+        this.studentAddress = studentAddress;
+    }
+
+    public Student() {
+        super();
+    }
+
+    public int getSutdentId() {
+        return sutdentId;
+    }
+
+    public void setSutdentId(int sutdentId) {
+        this.sutdentId = sutdentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "sutdentId=" + sutdentId +
+                ", studentName='" + studentName + '\'' +
+                ", studentAddress='" + studentAddress + '\'' +
+                '}';
+    }
 }
