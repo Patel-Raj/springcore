266a03be5d767cf63bf7ebecbfd5e49859c70808
Raj Patel <patelraj22777@gmail.com>
2022-01-03 12:10:56 +0530

    Added List using @Value

--- src/main/java/com/springcore/stereotype/Book.java
+++ src/main/java/com/springcore/stereotype/Book.java
@@ -4,16 +4,30 @@ package com.springcore.stereotype;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component("bk")
 public class Book {
 
     @Value("Atomic Habits")
     private String name;
 
+    @Value("#{lang}")
+    private List<String> languages;
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
                 "name='" + name + '\'' +
+                ", languages=" + languages +
                 '}';
     }
 
